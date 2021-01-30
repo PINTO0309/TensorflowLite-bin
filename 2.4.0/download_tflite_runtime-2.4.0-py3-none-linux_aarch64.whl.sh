34894e89ae6f59bf8420c3211c97c706b9e7acf8
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2021-01-30 14:05:29 +0900

    Support for custom operations in MediaPipe

--- 2.4.0/download_tflite_runtime-2.4.0-py3-none-linux_aarch64.whl.sh
+++ 2.4.0/download_tflite_runtime-2.4.0-py3-none-linux_aarch64.whl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1bOK1s2nS7V9eXcfHq9LYl3b6xPF-Sc7D" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=18GkcYrthUUhukWYuRO_S5ns5mJ9kaiG0" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1bOK1s2nS7V9eXcfHq9LYl3b6xPF-Sc7D" -o tflite_runtime-2.4.0-py3-none-linux_aarch64.whl
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=18GkcYrthUUhukWYuRO_S5ns5mJ9kaiG0" -o tflite_runtime-2.4.0-py3-none-linux_aarch64.whl
 
 echo Download finished.
