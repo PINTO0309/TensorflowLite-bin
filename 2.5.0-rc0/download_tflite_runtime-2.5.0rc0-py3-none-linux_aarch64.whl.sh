080f8b3cb59fb75cbeb147c95a3d0474a5137bee
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2021-04-03 01:34:47 +0900

    v2.5.0-rc0

---
+++ 2.5.0-rc0/download_tflite_runtime-2.5.0rc0-py3-none-linux_aarch64.whl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=15kbKiyCpY_PPkjCbRcChztujRbBlbazS" > /dev/null
+CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=15kbKiyCpY_PPkjCbRcChztujRbBlbazS" -o tflite_runtime-2.5.0rc0-cp37-none-linux_aarch64.whl
+
+echo Download finished.
+https://drive.google.com/file/d//view?usp=sharing
