30a120465b2f3980d72d0dff54bd7ef6fbac79c4
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2021-05-17 01:22:31 +0900

    Update download_tflite_runtime-2.5.0-cp37-none-linux_aarch64.whl.sh

--- 2.5.0/download_tflite_runtime-2.5.0-cp37-none-linux_aarch64.whl.sh
+++ 2.5.0/download_tflite_runtime-2.5.0-cp37-none-linux_aarch64.whl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=111uqIY1Bur1yBEos8I1W83z4wR1MO_ii" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1FvtLZSZFQmV5Ars8BBYFnZIX4X7jg1lA" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=111uqIY1Bur1yBEos8I1W83z4wR1MO_ii" -o tflite_runtime-2.5.0-cp37-none-linux_aarch64.whl
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1FvtLZSZFQmV5Ars8BBYFnZIX4X7jg1lA" -o tflite_runtime-2.5.0-cp37-none-linux_aarch64.whl
 
 echo Download finished.
