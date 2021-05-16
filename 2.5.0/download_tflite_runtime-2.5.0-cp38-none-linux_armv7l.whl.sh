c787182d994a071f9303dffee1059b374e511954
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2021-05-17 07:06:35 +0900

    Update download_tflite_runtime-2.5.0-cp38-none-linux_armv7l.whl.sh

--- 2.5.0/download_tflite_runtime-2.5.0-cp38-none-linux_armv7l.whl.sh
+++ 2.5.0/download_tflite_runtime-2.5.0-cp38-none-linux_armv7l.whl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1V6oFhRG73cEBd2GydGbnmY9AW_jOeHZb" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=16i_bBRhEldAPou1t1aJLsl351i-MsuGt" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1V6oFhRG73cEBd2GydGbnmY9AW_jOeHZb" -o tflite_runtime-2.5.0-cp38-none-linux_armv7l.whl
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=16i_bBRhEldAPou1t1aJLsl351i-MsuGt" -o tflite_runtime-2.5.0-cp38-none-linux_armv7l.whl
 
 echo Download finished.
