b22274612b74eaf0adfa05e5ace7d6a07961b53e
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2022-02-09 08:11:56 +0900

    Update download_tflite_runtime-2.8.0-cp38-none-linux_armv7l.whl.sh

--- 2.8.0/download_tflite_runtime-2.8.0-cp38-none-linux_armv7l.whl.sh
+++ 2.8.0/download_tflite_runtime-2.8.0-cp38-none-linux_armv7l.whl.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1b1fpv8WpKYjM6B8vxdGTDfKhqMHHMySi" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1b1fpv8WpKYjM6B8vxdGTDfKhqMHHMySi" -o tflite_runtime-2.8.0-cp38-none-linux_armv7l.whl
+curl -L https://github.com/PINTO0309/TensorflowLite-bin/releases/download/v2.8.0/tflite_runtime-2.8.0-cp38-none-linux_armv7l.whl -o tflite_runtime-2.8.0-cp38-none-linux_armv7l.whl
 
 echo Download finished.
