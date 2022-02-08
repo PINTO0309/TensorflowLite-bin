03f23a44dec062983c25e2e7cfba04d3006a2cac
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2022-02-09 08:11:17 +0900

    Update download_tflite_runtime-2.8.0-cp37-none-linux_aarch64.whl.sh

--- 2.8.0/download_tflite_runtime-2.8.0-cp37-none-linux_aarch64.whl.sh
+++ 2.8.0/download_tflite_runtime-2.8.0-cp37-none-linux_aarch64.whl.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 curl -L https://github.com/PINTO0309/TensorflowLite-bin/releases/download/v2.8.0/tflite_runtime-2.8.0-cp37-none-linux_aarch64.whl -o tflite_runtime-2.8.0-cp37-none-linux_aarch64.whl
+
 echo Download finished.
