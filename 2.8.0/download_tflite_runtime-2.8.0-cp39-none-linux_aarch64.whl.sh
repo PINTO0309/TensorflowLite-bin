2a0a9b5929f95b331bd9cf2c6d357ef300bb49a9
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2022-02-09 08:12:14 +0900

    Update download_tflite_runtime-2.8.0-cp39-none-linux_aarch64.whl.sh

--- 2.8.0/download_tflite_runtime-2.8.0-cp39-none-linux_aarch64.whl.sh
+++ 2.8.0/download_tflite_runtime-2.8.0-cp39-none-linux_aarch64.whl.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=13pl2U4Yd_IGoyxc4FVd4Iwz-i5v-k38Q" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=13pl2U4Yd_IGoyxc4FVd4Iwz-i5v-k38Q" -o tflite_runtime-2.8.0-cp39-none-linux_aarch64.whl
+curl -L https://github.com/PINTO0309/TensorflowLite-bin/releases/download/v2.8.0/tflite_runtime-2.8.0-cp39-none-linux_aarch64.whl -o tflite_runtime-2.8.0-cp39-none-linux_aarch64.whl
 
 echo Download finished.
