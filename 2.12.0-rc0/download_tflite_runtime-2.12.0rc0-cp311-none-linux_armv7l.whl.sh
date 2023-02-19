7eacb8479c10d9688f92bb3bdebf8867d041a6cd
pinto0309 <rmsdh122@yahoo.co.jp>
2023-02-19 10:49:43 +0900

    script

---
+++ 2.12.0-rc0/download_tflite_runtime-2.12.0rc0-cp311-none-linux_armv7l.whl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+TFVER=2.12.0-rc0
+curl -OL https://github.com/PINTO0309/TensorflowLite-bin/releases/download/v${TFVER}/tflite_runtime-${TFVER/-/}-cp311-none-linux_armv7l.whl
+
+echo Download finished.
