687f5f123dd7b29225a8e4491dd32f05aaa6097c
pinto0309 <rmsdh122@yahoo.co.jp>
2023-05-12 10:08:47 +0900

    v2.12.0

---
+++ 2.12.0/download_tflite_runtime-2.12.0-cp310-none-linux_aarch64.whl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+TFVER=2.12.0
+curl -OL https://github.com/PINTO0309/TensorflowLite-bin/releases/download/v${TFVER}/tflite_runtime-${TFVER/-/}-cp310-none-linux_aarch64.whl
+
+echo Download finished.
