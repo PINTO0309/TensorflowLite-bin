9bbd89d596713ccbb7976bdc1d2f8bdb499a0f8e
pinto0309 <rmsdh122@yahoo.co.jp>
2023-12-16 13:38:12 +0900

    .post1

--- 2.15.0/download_tflite_runtime-2.15.0-cp39-none-linux_armv7l.whl.sh
+++ 2.15.0/download_tflite_runtime-2.15.0-cp39-none-linux_armv7l.whl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TFVER=2.15.0
+TFVER=2.15.0.post1
 curl -OL https://github.com/PINTO0309/TensorflowLite-bin/releases/download/v${TFVER}/tflite_runtime-${TFVER/-/}-cp39-none-linux_armv7l.whl
 
 echo Download finished.
